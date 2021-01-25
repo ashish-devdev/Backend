9456bd74fd02ffab261779e8d41215f703408ba9
Prabhakaran <prabhakaran@devdencreativesolutions.onmicrosoft.com>
2021-01-25 17:13:25 +0530

    saved 3

--- model/arthematic_operation.model.js
+++ model/arthematic_operation.model.js
@@ -1,12 +1,11 @@
-const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let arthematic_operations = new Schema({
-    num1: {type: Int32, required: true, max: 12},
-    num2: {type: Int32, required: true, max: 12},
+    num1: {type:Number, required: true, max: 12},
+    num2: {type: Number , required: true, max: 12},
     operation: {type: String, required: true, max: 12},
-    result:{type:Int32  }
+    result:{type:Number  }
 
 });
 
